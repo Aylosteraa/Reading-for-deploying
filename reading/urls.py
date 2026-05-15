72dc6aed8082a32027302bfbd835edf3895d8796
Aylosteraa <barabashmarinaaa@gmail.com>
2026-05-15 13:52:07 +0300

    Add robots.txt

--- reading/urls.py
+++ reading/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,13 @@ urlpatterns = [
     path('creating/', include('creating.urls')),
     path('forum/', include('forum.urls')),
     path('chat/', include('chat.urls')),
+    path(
+    "robots.txt",
+    TemplateView.as_view(
+        template_name="robots.txt",
+        content_type="text/plain"
+    ),
+),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
